68a493e113c3f4d642fc4864bcf8f91947d2aed2
hybfkuf <fredjoseph863@gmail.com>
2022-07-26 09:41:22 +0800

    Support init logger from aguments

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
@@ -9,62 +8,55 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func New() *logrus.Logger {
-	logger := logrus.New()
-
+func Init() {
 	logLevel := args.GetLogLevel()
 	logFormat := args.GetLogFormat()
 	logFile := args.GetLogFile()
-	fmt.Println(logLevel)
-	fmt.Println(logFormat)
-	fmt.Println(logFile)
 
 	// set log output, default is os.Stdout.
 	switch strings.ToUpper(logLevel) {
 	case "ERROR":
-		logger.SetLevel(logrus.ErrorLevel)
+		logrus.SetLevel(logrus.ErrorLevel)
 	case "WARN":
-		logger.SetLevel(logrus.WarnLevel)
+		logrus.SetLevel(logrus.WarnLevel)
 	case "WARNING":
-		logger.SetLevel(logrus.WarnLevel)
+		logrus.SetLevel(logrus.WarnLevel)
 	case "INFO":
-		logger.SetLevel(logrus.InfoLevel)
+		logrus.SetLevel(logrus.InfoLevel)
 	case "DEBUG":
-		logger.SetLevel(logrus.DebugLevel)
+		logrus.SetLevel(logrus.DebugLevel)
 	case "TRACE":
-		logger.SetLevel(logrus.TraceLevel)
+		logrus.SetLevel(logrus.TraceLevel)
 	default:
-		logger.SetLevel(logrus.InfoLevel)
+		logrus.SetLevel(logrus.InfoLevel)
 	}
 
 	// set log format, default is text format.
 	switch strings.ToUpper(logFormat) {
 	case "TEXT":
-		//logger.SetFormatter(&logrus.TextFormatter{})
-		logger.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetFormatter(&logrus.TextFormatter{})
 	case "JSON":
-		logger.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetFormatter(&logrus.JSONFormatter{})
 	default:
-		logger.SetFormatter(&logrus.TextFormatter{})
+		logrus.SetFormatter(&logrus.TextFormatter{})
 	}
 
 	// set log file, default is os.Stdout.
 	switch logFile {
 	case "/dev/stdout":
-		logger.SetOutput(os.Stdout)
+		logrus.SetOutput(os.Stdout)
 	case "/dev/stderr":
-		logger.SetOutput(os.Stderr)
+		logrus.SetOutput(os.Stderr)
 	default:
 		file, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 		if err != nil {
 			panic(err)
 		}
-		logger.SetOutput(file)
+		logrus.SetOutput(file)
 	}
 
-	//// SetReportCaller sets whether the standard logger will include the calling
+	//// SetReportCaller sets whether the standard logrus will include the calling
 	//// method as a field.
-	//logger.SetReportCaller(false)
+	//logrus.SetReportCaller(false)
 
-	return logger
 }
