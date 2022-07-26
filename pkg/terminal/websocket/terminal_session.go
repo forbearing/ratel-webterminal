091ffd88a4ee93d4133461ec88936d5599517ec0
hybfkuf <fredjoseph863@gmail.com>
2022-07-26 20:07:13 +0800

    Add chinese comments

--- pkg/terminal/websocket/terminal_session.go
+++ pkg/terminal/websocket/terminal_session.go
@@ -35,7 +35,14 @@ func NewTerminalSession(w http.ResponseWriter, r *http.Request, responseHeader h
 	return session, nil
 }
 
-// Next called in a loop from remotecommand as long as the process is running
+// remotecommand 包会启用 goroutine 调用 Next() 方法, 这个方法会一直阻塞来获取通道内容,
+
+// sizeCh 包含了前端 TypeScript 发送过来的有关浏览器的长宽大小的信息, 一旦从 sizeCh
+// 通道获取到了数据, remotecommand 包就会相应调整 pod 容器的 terminal 大小.
+
+// 如果从 doneCh 获取到内容, 表明用户刷新了浏览器的 web终端或者其他 pod 容器的其他原因,
+// 或者是网络原因, TerminalSession 将会关闭其内部 websocket, remotecommand 包
+// 也会断开和 pod 容器建立的双向 shell streams 长连接.
 func (t *TerminalSession) Next() *remotecommand.TerminalSize {
 	select {
 	case size := <-t.sizeCh:
@@ -45,7 +52,24 @@ func (t *TerminalSession) Next() *remotecommand.TerminalSize {
 	}
 }
 
-// Read called in a loop from remotecommand as long as the process is running
+// Read 从 TerminalSession 内部的 websocket 中读取数据, 并将读取到的输出作为容器的 stdin
+
+// 1.remotecommand.NewSPDYExecutor() 函数创建一个 Executor 来和 pod 容器建立一个
+//   多路复用的双向 shell streams 连接.
+// 2.remotecommand 包通过 Executor.Stream() 选项来设置 pod 容器的 stdin, stdout, stderr
+//   这里传入的是一个 TerminalSession 对象,
+// 3.也就是说, pod 容器的 stdin 是来自 TerminalSession 内部的 websocket.ReadMessage()
+//   pod 容器 stdout, stderr 会通过调用 TerminalSession 内部的 websocket 的 WriteMessage()
+//   方法将 pod 容器的 stdout, stder 内容写入到 TerminalSession 内部的 websocket 中
+
+// 1.前端 TypeScript 代码通过 conn.send() 函数将用户在浏览器 web 终端输出的 shell 命令
+//   发送到 TerminalSession 的 websocket 连接中, 并将 Op 标志设置为 "stdin"
+// 2.检查 Op 标志, 如果为 "stdin", 将 TerminalSession 内部 websocket 中的内容
+//   作为 pod 容器的 stdin 发送给 pod 容器, 来执行用户输入的 shell 命令
+//   具体代码见 ./frontend/terminal.js 34,35,46,47 行.
+// 3.检查 Op 标志, 如果为 "resize", remotecommand 则会根据前端 TypeScript 代码传来的
+//   浏览器长宽大小来调整容器的 Terminal 大小.
+//   具体代码将 ./frontend/terminal.js 39,40 行.
 func (t *TerminalSession) Read(p []byte) (int, error) {
 	_, message, err := t.conn.ReadMessage()
 	if err != nil {
@@ -77,7 +101,17 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 	}
 }
 
-// Write called from remotecommand whenever there is any output
+// Write 将 pod 容器的任何 stdout 和 stderr 输出写入到 TerminalSession 内部的 websocket 中,
+// 前端 TypeScript 代码从 TerminalSession 内部的 websocket 读取数据并输出到 浏览器的 web terminal 中.
+
+// 1.remotecommand 通过 Executor.Stream() 方法设置好了 pod 容器的 stdout, stderr.
+// 2.remotecommand 可以获取 pod 容器的任何 stdout, stderr 输出, 并将 stdout, stderr
+//   输出内容写入到 TerminalSession 内部的 websocket 中. 并将 Op 标志设置为 "stdout".
+// 3.前端 TypeScript 代码检查 Op 标志是否为 "stdout", 如果是 "stdout", 则将通过
+//   term.write(msg.data) 函数将 TerminalSession 内部 websocket 的内容输出到浏览器
+//   的 web 终端上.
+//
+// 具体代码见 ./frontend/terminal.js 的 52,53 行.
 func (t *TerminalSession) Write(p []byte) (int, error) {
 	msg, err := json.Marshal(TerminalMessage{
 		Op:   "stdout",
@@ -94,7 +128,11 @@ func (t *TerminalSession) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-// Close close session
+// 浏览器 web 终端重新刷新了, 集群 pod 容器故障, 或者其他网络原因等, 将会关闭一个 TerminalSession
+
+// Close 函数将会关闭 TerminalSession 内部的 websocket, 并关闭 doneCh 通道.
+// remotecommand 包调用的 Next() 函数感知到 doneCh 通道关闭了,也就会关闭与 pod 容器
+// 建立的双向 shell streams 长连接.
 func (t *TerminalSession) Close() error {
 	close(t.doneCh)
 	return t.conn.Close()
--- pkg/terminal/websocket/websocket.go
+++ pkg/terminal/websocket/websocket.go
@@ -29,14 +29,90 @@ func HandleLogs(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./frontend/logs.html")
 }
 
+// 前端为 xterm.js, 前端代码的主要函数功能
+//    term.write("message"):   将 message 信息写到浏览器上
+//    conn.send():             向 websocket 中写数据
+
 // HandleWsTerminal
+
+// 这里主要有三个数据结构: TerminalMessage, TerminalSession, PtyHandler
+// TerminalMessage 相当于一个协议, 支持支持3种, stdin, stdout, resize
+
+// TerminalSession 包含三个字段: conn, sizeCh, doneCh
+//     conn 就是一个 websocket 连接
+//     sizeCh 是一个 remotecommand.TerminalSize 类型, 该类型包含了浏览器的长宽信息
+//     doneCh 是一ge channel 类型, 用来标记 TerminalSession 是否关闭了.
+
+// TerminalSession 对象有4个方法, Read(), Write(), Next(), Close().
+//     Read() 用来从 TerminalSession 的 websocket 读取数据, 实现了 io.Reader 接口
+//     Write() 用来从 TerminalSession 的 websocket 写数据, 实现了 io.Writer 接口
+//     Next() 实现了 remotecommand.TerminalSizeQueue 接口
+// PtyHandler 是一个接口, 其包含了三个子接口, 分别是: io.Reader, io.Writer, remotecommand.TerminalSizeQueue
+// 所以 TerminalSession 对象实现了 PtyHandler 接口.
+
+// 每一个浏览器终端都会使用一个 TerminalSession
+
+// podHandler.Execute(podName, containerName, []string{"bash"}, pty) 会调用以下这段代码.
+//exec, err := remotecommand.NewSPDYExecutor(h.config, "POST", req.URL())
+//if err != nil {
+//    return err
+//}
+//return exec.Stream(remotecommand.StreamOptions{
+//    Stdin:             pty,
+//    Stdout:            pty,
+//    Stderr:            pty,
+//    TerminalSizeQueue: pty,
+//    Tty:               true,
+//})
+// 如何理解这段代码:
+// 1.remotecommand.NewSPDYExecutor 会与 pod 容器建立连接,并将连接升级到一个多路复用流连接.
+// 2.exec.Stream(remotecommand.StreamOptions{}) 会建立一个标准的 shell streams, 并设置
+//   容器的 stdin, stdout, stderr
+//   这里传入的是一个 PtyHandler 接口的实例, 其实是一个 TerminalSession 的对象.
+//   因为 TerminalSession 实现了 PtyHandler 接口(上面已经讲了).
+//   remotecommand 会调用 TerminalSession 的 Write 方法将 pod 容器的任何输出内容,
+//   将通过 TerminalSession 的 Write 方法写入到 TerminalSession 的 websocket 连接中
+//   然后前端 TypeScript 代码会读取 websocket, 将 pod 容器的输出反映到浏览器 web terminal 上.
+
+// pod 容器输出:
+// 1.remotecommand.NewSPDYExecutor() 函数创建一个 Executor, 将会和 pod 容器连理一个多路复用的双向流连接.
+// 2.remotecommand 调用 Executor.Stream 方法来初始化 pod 容器的 stdin, stdout, stderr.
+//   这里是用一个 TerminalSession 对象来初始化 pod 容器的 stdin, stdout, stderr.
+// 3.pod 容器的任何输出, 都会被 remotecommand 调用 TerminalSession 对象的 Write 方法
+//   写入到 TerminalSession 内部的一个 websocket 连接中.
+// 4.前端 TypeScript 代码会 websocket 读取内容并将这些内容反映到浏览器的 web terminal 上.
+
+// pod 容器获取用户输出的命令
+// 1.前端 TypeScript 代码会将用户在浏览器输入的或复制的 shell 命令写入 TerminalSession
+//   的 websocket 连接中, remotecommand 调用 TerminalSession 的 Read 方法将
+//
+
+// 具体流程
+// 1.前端 TypeScript 代码会将用户在浏览器输入的或复制的 shell 命令, 写入到 TerminalSession 中
+//   具体代码为 ./frontend/terminal.js 的第 34,35 行代码
+//   协议为 TerminalMessage, 其中 Data 包含了具体的传输内容, Op 是一个标志, 这里为 stdin
+//   表示向 TerminalSession 写内容,
+//   向 Stream 中写数据, TerminalMessage 的 Data 包含了具体的内容, Op 是一个标志
+
+// 1.TypeScript 代码将用户输出的或复制的 shell 命令写入到 remotecommand 建立的
+//   长连接中, Op 为 "stdin". remotecommand 会调用 TerminalSession 的 Read 方法
+//   将 shell 命令传给容器执行.
+//   具体 TypeScript 代码为 ./frontend/terminal.js 的 34,35,46,47 行.
+// 2.remotecommand 包会将容器的 shell 命令输出信息, 通过调用 TerminalSession 的
+//   Write 方法写给前端 TypeScript 并附带 Op 为 stdout 的标志. 前端 TypeScript 代码
+//   检查 Op 标志, 如果 Op 标志为 "stdout", 则将容器的输出写入到浏览器.
+//   具体 TypeScript 代码为 ./frontend/terminal.js 的 52,53 行.
+// 3.浏览器的尺寸发生变化, TypeScript 代码会将浏览器的持续信息写入到 remotecommand
+//   建立的长连接中,并且将 Op 标志设置为 "resize", remotecommand 就会调整容器
+//   terminal 的大小.
+//   具体 TypeScript 代码为 ./frontend/terminal.js 的 38,39 行代码.
+
 func HandleWsTerminal(w http.ResponseWriter, r *http.Request) {
 	pathParams := mux.Vars(r)
 	namespace := pathParams["namespace"]
 	podName := pathParams["pod"]
 	containerName := pathParams["container"]
 	log.Infof("exec pod: %s/%s, container: %s", namespace, podName, containerName)
-	log.Info(r.URL)
 
 	pty, err := NewTerminalSession(w, r, nil)
 	if err != nil {
@@ -63,17 +139,17 @@ func HandleWsTerminal(w http.ResponseWriter, r *http.Request) {
 // 实际在浏览器中访问的路径为: http://localhost:8080/logs?namespace=default&pod=nginx&container=nginx
 // 注意这里的 uri 和我们实际的 api 不同. 为什么呢?
 // 因为当用浏览器访问 http://localhost:8080/logs?namespace=default&pod=nginx&container=nginx 时,
-// 前端 javascript 代码会通过这个 uri 合成一个新的 uri, 然后前端的 javascript 脚本会调用
+// 前端 TypeScript 代码会通过这个 uri 合成一个新的 uri, 然后前端的 TypeScript 脚本会调用
 // 我们的 RESTful API, 也就是 /ws/{namespace}/{pod}/{container}/logs
 
-// 前端 javascript 代码关键行在 frontend/logs.js 的 25行 和 45行.
+// 前端 TypeScript 代码关键行在 frontend/logs.js 的 25行 和 45行.
 // 25行处理 http://localhost:8080/logs?namespace=default&pod=nginx&container=nginx 请求生成一个新的 uri
 // 45行根据新的 uri 调用 ratel-webterminal 的 RESTful API, 也就是 /ws/{namespace}/{pod}/{container}/logs
 
 // 总结:
-// 1.前端的 javascript 代码会转换浏览器请求的 uri
+// 1.前端的 TypeScript 代码会转换浏览器请求的 uri
 //   比如 http://localhost:8080/logs?namespace=default&pod=nginx&container=nginx
-// 2.前端的 javascript 代码再调用 ratel-webtermal 的 api,
+// 2.前端的 TypeScript 代码再调用 ratel-webtermal 的 api,
 //   也就是这里的 /ws/{namespace}/{pod}/{container}/logs
 func HandleWsLogs(w http.ResponseWriter, r *http.Request) {
 	pathParams := mux.Vars(r)
@@ -82,7 +158,6 @@ func HandleWsLogs(w http.ResponseWriter, r *http.Request) {
 	containerName := pathParams["container"]
 	tailLines, _ := strconv.ParseInt(r.URL.Query().Get("tail"), 10, 64)
 	log.Infof("get pod logs: %s/%s, container: %s, tailLines: %d\n", namespace, podName, containerName, tailLines)
-	log.Info(r.URL)
 
 	writer, err := NewLogger(w, r, nil)
 	if err != nil {
@@ -108,7 +183,7 @@ func HandleWsLogs(w http.ResponseWriter, r *http.Request) {
 	// 1.podHandler.Log() 指定 pod 的日志, 默认日志是写入到标准输出, 但是我们通过
 	//   logOptions 设置了 Writer 字段为我们指定的 writer. 也就是说会将 pod 的日志
 	//   远远不断的写入到 writer 对象封装的 websocket 连接中.
-	// 2.前端的 javascript 脚本会读取 websocket 中的 pod 日志.
+	// 2.前端的 TypeScript 脚本会读取 websocket 中的 pod 日志.
 	//   然后我们就可以在浏览器中查看到这个 pod 的日志.
 
 	var logOptions pod.LogOptions
