06a064800c4718ba0fa691dc5a22afd839d75c10
hybfkuf <fredjoseph863@gmail.com>
2022-07-26 11:22:36 +0800

    Add healthcheck and readycheck http handle function

---
+++ pkg/terminal/probe.go
@@ -0,0 +1,21 @@
+package terminal
+
+import "net/http"
+
+// HandleHealthProbe handle api "/-/health"
+// it alwasy response wth 200 status code and message "ok" if the ratel-webterminal is running.
+func HandleHealthProbe(w http.ResponseWriter, r *http.Request) {
+	// you should alwasy call w.WriteHeader before anything else it will output
+	// some unexpected message like "http: superfluous response.WriteHeader call from github.com...."
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+// HandleReadyProbe handle api "/-/ready"
+// it alwasy response wth 200 status code and message "ok" if the ratel-webterminal is running.
+func HandleReadyProbe(w http.ResponseWriter, r *http.Request) {
+	// you should alwasy call w.WriteHeader before anything else it will output
+	// some unexpected message like "http: superfluous response.WriteHeader call from github.com...."
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
