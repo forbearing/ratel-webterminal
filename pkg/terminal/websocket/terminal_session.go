723f4830d87d9a8c3cfdbf5f9977078ec48861af
hybfkuf <fredjoseph863@gmail.com>
2022-07-27 13:54:54 +0800

    Update chinese comments

--- pkg/terminal/websocket/terminal_session.go
+++ pkg/terminal/websocket/terminal_session.go
@@ -21,7 +21,8 @@ func NewTerminalSessionWs(conn *websocket.Conn) *TerminalSession {
 	}
 }
 
-// NewTerminalSession create TerminalSession
+// NewTerminalSession 创建一个 TerminalSession 对象,  同时将 http 连接升级到 websocket 并放入 TerminalSession 对象.
+// 后续前端 JavaScript 代码可以向 TerminalSession 内部维护的 websocket 写数据和读取数据
 func NewTerminalSession(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*TerminalSession, error) {
 	conn, err := upgrader.Upgrade(w, r, responseHeader)
 	if err != nil {
@@ -35,42 +36,34 @@ func NewTerminalSession(w http.ResponseWriter, r *http.Request, responseHeader h
 	return session, nil
 }
 
-// remotecommand 包会启用 goroutine 调用 Next() 方法, 这个方法会一直阻塞来获取通道内容,
-
-// sizeCh 包含了前端 TypeScript 发送过来的有关浏览器的长宽大小的信息, 一旦从 sizeCh
-// 通道获取到了数据, remotecommand 包就会相应调整 pod 容器的 terminal 大小.
-
-// 如果从 doneCh 获取到内容, 表明用户刷新了浏览器的 web终端或者其他 pod 容器的其他原因,
-// 或者是网络原因, TerminalSession 将会关闭其内部 websocket, remotecommand 包
-// 也会断开和 pod 容器建立的双向 shell streams 长连接.
+// remotecommand 会循环调用 Next() 方法
 func (t *TerminalSession) Next() *remotecommand.TerminalSize {
 	select {
+	// sizeCh 包含了前端 TypeScript 发送过来的有关浏览器的长宽大小的信息, 一旦从 sizeCh
+	// 通道获取到了数据, remotecommand 包就会相应调整 pod 容器的 terminal 大小.
 	case size := <-t.sizeCh:
 		return &size
+
+		// 如果从 doneCh 被关闭了, 表明用户刷新了浏览器的 web 终端或者其他 pod 容器的其他原因,
+		// 或者是网络原因, TerminalSession 将会关闭其内部 websocket, remotecommand 包
+		// 也会断开和 pod 容器建立的双向 shell streams 长连接.
 	case <-t.doneCh:
 		return nil
 	}
 }
 
-// Read 从 TerminalSession 内部的 websocket 中读取数据, 并将读取到的输出作为容器的 stdin
+// remotecommand.NewSPDYExecutor() 函数创建一个 Executor 来和 pod 容器建立一个双向的 shell streams 连接.
+// 再通过 Executor.Stream() 方法设置好了 pod 容器的 stdin, stdout, stderr, 最终效果是:
+// 1. pod 容器的任何输入都来自 TerminalSession 内部维护的 websocket.
+// 2. pod 容器的任何输出都会写入 TerminalSession 内部维护的 websocket.
 
-// 1.remotecommand.NewSPDYExecutor() 函数创建一个 Executor 来和 pod 容器建立一个
-//   多路复用的双向 shell streams 连接.
-// 2.remotecommand 包通过 Executor.Stream() 选项来设置 pod 容器的 stdin, stdout, stderr
-//   这里传入的是一个 TerminalSession 对象,
-// 3.也就是说, pod 容器的 stdin 是来自 TerminalSession 内部的 websocket.ReadMessage()
-//   pod 容器 stdout, stderr 会通过调用 TerminalSession 内部的 websocket 的 WriteMessage()
-//   方法将 pod 容器的 stdout, stder 内容写入到 TerminalSession 内部的 websocket 中
-
-// 1.前端 TypeScript 代码通过 conn.send() 函数将用户在浏览器 web 终端输出的 shell 命令
-//   发送到 TerminalSession 的 websocket 连接中, 并将 Op 标志设置为 "stdin"
-// 2.检查 Op 标志, 如果为 "stdin", 将 TerminalSession 内部 websocket 中的内容
-//   作为 pod 容器的 stdin 发送给 pod 容器, 来执行用户输入的 shell 命令
-//   具体代码见 ./frontend/terminal.js 34,35,46,47 行.
-// 3.检查 Op 标志, 如果为 "resize", remotecommand 则会根据前端 TypeScript 代码传来的
-//   浏览器长宽大小来调整容器的 Terminal 大小.
-//   具体代码将 ./frontend/terminal.js 39,40 行.
+// 1.Read 方法用来从 TerminalSession 内部维护的 websocket 读取数据.
+// 2.remotecommand 包会调用 TerminalSession 的 Read 方法来获取数据, 作为 pod 容器的 stdin
+// 3.前端 JavaScript 代码将用户在浏览器 web 终端上输入的 shell 指令写入到 TerminalSession 内部的 websocket.
+// 4.最终 pod 容器知道要执行哪个命令.
 func (t *TerminalSession) Read(p []byte) (int, error) {
+	// 调用 websocket.ReadMessage() 方法从 websocket 读取数据, 数据类型主要有两种
+	// 一种是用户输入的 shell 指令, 一种是浏览器长宽大小信息.
 	_, message, err := t.conn.ReadMessage()
 	if err != nil {
 		if errors.Is(err, net.ErrClosed) {
@@ -80,16 +73,23 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 			log.Printf("read message err: %v", err)
 			return copy(p, END_OF_TRANSMISSION), err
 		}
-		//return copy(p, EndOfTransmission), err
 	}
 	var msg TerminalMessage
 	if err := json.Unmarshal([]byte(message), &msg); err != nil {
 		log.Printf("read parse message err: %v", err)
 		return copy(p, END_OF_TRANSMISSION), err
 	}
+
+	// 如果 Op 标志位为 stdin, 表示是用户输入的 shell 指令
+	// 具体前端 JavaScript 代码为 ./frontend/terminal.js 34, 35 行
 	switch msg.Op {
 	case "stdin":
 		return copy(p, msg.Data), nil
+
+	// 如果 Op 标志位为 resize, 表示是浏览器长宽大小信息.
+	// 则向 sizeCh 通道发送当前浏览器长宽大小信息, remotecommand 包会循环调用 TerminalSession 的 Next() 方法
+	// 在该 Next() 方法中, remotecommand 包接收到了浏览器新的长宽大小, 就会调整 pod 容器的终端大小.
+	// 具体前端 JavaScript 代码为 "./frontend/terminal.js" 的 39,40行
 	case "resize":
 		t.sizeCh <- remotecommand.TerminalSize{Width: msg.Cols, Height: msg.Rows}
 		return 0, nil
@@ -99,17 +99,16 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 	}
 }
 
-// Write 将 pod 容器的任何 stdout 和 stderr 输出写入到 TerminalSession 内部的 websocket 中,
-// 前端 TypeScript 代码从 TerminalSession 内部的 websocket 读取数据并输出到 浏览器的 web terminal 中.
+// remotecommand.NewSPDYExecutor() 函数创建一个 Executor 来和 pod 容器建立一个双向的 shell streams 连接.
+// 再通过 Executor.Stream() 方法设置好了 pod 容器的 stdin, stdout, stderr, 最终效果是:
+// 1.pod 容器的任何输入都来自 TerminalSession 内部维护的 websocket.
+// 2.pod 容器的任何输出都会写入 TerminalSession 内部维护的 websocket.
 
-// 1.remotecommand 通过 Executor.Stream() 方法设置好了 pod 容器的 stdout, stderr.
-// 2.remotecommand 可以获取 pod 容器的任何 stdout, stderr 输出, 并将 stdout, stderr
-//   输出内容写入到 TerminalSession 内部的 websocket 中. 并将 Op 标志设置为 "stdout".
-// 3.前端 TypeScript 代码检查 Op 标志是否为 "stdout", 如果是 "stdout", 则将通过
-//   term.write(msg.data) 函数将 TerminalSession 内部 websocket 的内容输出到浏览器
-//   的 web 终端上.
-//
-// 具体代码见 ./frontend/terminal.js 的 52,53 行.
+// 1.Write 方法用来将数据写入到 TerminalSession 内部维护的 websocket.
+// 2.remotecommand 包会调用 TerminalSession 的 Write 方法将 pod 容器的任何 stdout, stderr 输出写入到 TerminalSession
+//   内部维护的 websocket
+// 3.前端 JavaScript 代码从 TerminalSession 内部维护的 websocket 读取数据并输出到浏览器的 web terminal 上.
+// 4.最终用户在 web 终端上得到自己命令的输出结果.
 func (t *TerminalSession) Write(p []byte) (int, error) {
 	msg, err := json.Marshal(TerminalMessage{
 		Op:   "stdout",
--- pkg/terminal/websocket/type.go
+++ pkg/terminal/websocket/type.go
@@ -11,21 +11,51 @@ import (
 
 const END_OF_TRANSMISSION = "\u0004"
 
-// PtyHandler is what remotecommand expects from a pty
+// PtyHandler 是一个接口包含了三个子接口: io.Reader, io.Writer, remotecommand.TerminalSizeQueue
+// io.Reader 接口有一个 Read() 方法
+// io.Writer 接口有一个 Write() 方法
+// remotecommand.TerminalSizeQueue 接口有一个 Next() 方法
+// TerminalSession 对象有: Read(), Write(), Next() 方法, 所有 TerminalSession 对象实现了 PtyHandler 接口
+
+// remotecommand 包的 NewSPDYExecutor() 函数会创建一个 Executor 对象来和 pod 容器建立双向的 shell streams 长连接.
+// 再通过 Executor.Stream() 方法用一个 PtyHandler 实例来设置 pod 容器的 stdin, stdout, stderr. 这个 PtyHandler 实例
+// 也就是 TerminalSession 对象.
+
+// 当 remotecommand 与 pod 容器建立好双向的 shell streams 长连接, 并用一个 TerminalSession 设置好 pod 容器的 stdin,stdout,stderr 之后
+// 1. pod 容器的 stdin(), 来自 TerminalSession.Read() 方法. Read() 方法会从其内部的 websocket 中读取数据, 也就是浏览器
+//    web 终端中用户输入的 shell 指令.
+// 2. pod 容器的 stdout, stderr 会通过 TerminalSession.Write() 方法写入到其内部的 websocket. websocket 会被前端 JavaScript
+//    代码读取并输出到浏览器 web 终端上
+// 3. 最终用户看到了自己 shell 指令的输出结果.
 type PtyHandler interface {
-	remotecommand.TerminalSizeQueue
 	io.Reader
 	io.Writer
+	remotecommand.TerminalSizeQueue
 }
 
-// TerminalSession implements PtyHandler
+// TerminalSession 三个字段/属性.
+// conn:    内部维护的一个 websocket 连接, NewTerminalSession() 函数可以把 http 连接升级到 websocket.
+//          升级得到的 websocket 就存放在这里.
+//          后续浏览器 JavaScript 会将用户的 shell 指令写入到该 websocket, 并调用 TerminalSession 的 Read() 方法写入到 pod 容器
+//          pod 容器的输出会调用 TerminalSession 的 Write() 方法写入到该 websocket, 前端 JavaScript 代码就可以从该 websocket
+//          读取容器的输出内容,并写到 web 终端浏览器上
+// sizeCh:  是一个 remotecommand.TerminalSize, 代表浏览器 web 终端的长宽大小
+// doneCh:  当 remotecommand 包与 pod 容器建立的双向 shell streams 长连接断开后(比如用户刷新浏览器等操作导致的),
+//          remotecommand 包会向 doneCh 发送一个空数据, TerminalSession 会关闭内部维护的 websocket
 type TerminalSession struct {
 	conn   *websocket.Conn
 	sizeCh chan remotecommand.TerminalSize
 	doneCh chan struct{}
 }
 
-// TerminalMessage is the messaging protocol between ShellController and TerminalSession.
+// TerminalMessage 是前端 JavaScript 代码和 TerminalSession 内部维护的 websocket 之间的通信协议.
+
+// Op:     标志位,用来标记通信数据的类型.
+//         如果为 stdin,  表示前端 JavaScript 代码将用户输出的 shell 指令发送到 TerminalSession 内部维护的 websocket.
+//         如果为 stdout, 表示前端 JavaScript 代码将从 TerminalSession 内部维护的 websocket 读取数据并输出到浏览器 web 终端上.
+//         如果为 resize, 表示前端 JavaScript 代码将浏览器到长宽大小信息发送到 TerminalSession 内部维护 websocket.
+// Data:   前端 JavaScript 代码从 TerminalSession 内部内部维护的 websocket 中写入或读取的数据, Op 为 stdin 或 stdout
+// Rows,Cols:  浏览器的长宽大小信息, Op 为 resize.
 type TerminalMessage struct {
 	Op   string `json:"op"`
 	Data string `json:"data"`
